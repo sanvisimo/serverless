4ee4e72fa0eb3c446dc523bf5172bab710413bef
Simone Sanvito <sanvi.simo@gmail.com>
2019-08-09 12:25:49 +0200

    Use environment variables in our functions

--- update.js
+++ update.js
@@ -4,19 +4,19 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
+    // - 'reviewId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      reviewId: event.pathParameters.id
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET content = :content, vote = :vote",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
+      ":vote": data.vote || null,
       ":content": data.content || null
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
